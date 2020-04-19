39de3ba0b33014763b5efe807033dcb35f5d1c7d
Rafael De Hermoso <rdehermoso@hotmail.com>
2020-04-18 20:30:20 -0500

    Character movement and collision camera

---
+++ Assets/Scripts/Scene-ThirdPersonController/ThirdPersonCameraController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraController : MonoBehaviour {
+    
+    public float rotationSpeed = 1.0f;
+    public Transform Target, Player;
+    float mouseX, mouseY;
+
+    public Transform Obstruction;
+    float zoomSpeed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start() {
+        Obstruction = Target;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void LateUpdate() {
+        camControl();
+        viewObstructed();
+    }
+
+    void camControl(){
+        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
+        mouseY += Input.GetAxis("Mouse Y") * rotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        transform.LookAt(Target);
+
+        Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        Player.rotation = Quaternion.Euler(0, mouseX, 0);
+    }
+
+    void viewObstructed(){
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Target.position - transform.position, out hit, 4.5f)) {
+            if(hit.collider.gameObject.tag != "Player") {
+                Obstruction = hit.transform;
+                Obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+
+                if (Vector3.Distance(Obstruction.position, transform.position) >= 3.0f && Vector3.Distance(transform.position, Target.position) >= 1.5f) {
+                    transform.Translate(Vector3.forward * zoomSpeed * Time.deltaTime);
+                }
+            }else {
+                Obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+                if (Vector3.Distance(transform.position, Target.position)< 4.5f) {
+                    transform.Translate(Vector3.back * zoomSpeed * Time.deltaTime);
+                }
+            }
+        }
+    }
+}
